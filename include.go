484596e89f067874e226e9c9eacb7760a52b7db0
Cheer Xiao <xiaqqaix@gmail.com>
2015-01-02 22:46:32 +0100

    genblog, a blog generator

---
+++ include.go
@@ -0,0 +1,225 @@
+package main
+
+const css = `/*
+Copied from:  http://code.google.com/p/html5resetcss/
+
+html5doctor.com Reset Stylesheet
+v1.6.1
+Last Updated: 2010-09-17
+Author: Richard Clark - http: //richclarkdesign.com
+Twitter: @rich_clark
+*/
+html, body, div, span, object, iframe, h1, h2, h3, h4, h5, h6, p,
+blockquote, pre, abbr, address, cite, code, del, dfn, em, img, ins,
+kbd, q, samp, small, strong, sub, sup, var, b, i, dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, figcaption, figure, footer, header, hgroup,
+menu, nav, section, summary, time, mark, audio,
+video { margin: 0; padding: 0; border: 0; outline: 0;
+            font-size: 100%; vertical-align: baseline; background: transparent; }
+body { line-height: 1; }
+article, aside, details, figcaption, figure, footer, header, hgroup, menu, nav,
+section { display: block; }
+nav ul { list-style: none; }
+blockquote, q { quotes: none; }
+blockquote:before, blockquote:after, q:before, q:after { content: ''; content: none; }
+a { margin: 0; padding: 0; font-size: 100%; vertical-align: baseline; background: transparent; }
+ins { background-color: #ff9; color: #000; text-decoration: none; }
+mark { background-color: #ff9; color: #000; font-style: italic; font-weight: bold; }
+del { text-decoration: line-through; }
+abbr[title], dfn[title] { border-bottom: 1px dotted; cursor: help; }
+table { border-collapse: collapse; border-spacing: 0; }
+hr { display: block; height: 1px; border: 0; border-top: 1px solid #cccccc; margin: 1em 0; padding: 0; }
+input, select { vertical-align: middle; }
+/* end HTML5 reset */
+` + `body {
+    background-color: #e5e5e5;
+    font-size: 20px;
+    font-family: Georgia, Times, Serif, FZShuSong-Z01, SimSun;
+}
+
+#page {
+    background-color: white;
+    padding: 24px 24px 20px 24px;
+    box-shadow: 2px 1px 3px #cccccc;
+}
+
+@media screen and (min-width: 768px) {
+    #page {
+        margin: 32px auto 20px auto;
+        padding: 48px 48px 40px 48px;
+        width: 672px;
+    }
+}
+
+#header {
+    font-size: 2em;
+    margin-bottom: 2rem;
+}
+
+#header > a, a.nav-link {
+    color: black;
+    text-decoration: none;
+}
+
+#category-list > li {
+    list-style: none;
+    display: inline;
+}
+
+#category-list, #article, #article-list {
+    margin-top: 1em;
+}
+
+.nav-link {
+    padding-bottom: 2px;
+    border-bottom: 2px solid #d0d0d0;
+}
+
+.nav-link.current, .nav-link.current:hover {
+    border-bottom-color: black;
+}
+
+.nav-link:hover {
+    border-bottom-color: #888888;
+}
+
+#category-list > li+li:before {
+    content: '· ';
+    color: #888888;
+}
+
+#article p {
+	line-height: 1.1;
+	margin-bottom: 0.6em;
+}
+
+/* Only use headers up to h3 */
+#article h1, #article h2, #article h3 {
+    font-family: Arial, Sans Serif, FZHei-B01, WenQuanYi Micro Hei, SimHei;
+    line-height: 1.5;
+    margin-top: 0.6em;
+    margin-bottom: 0.6em;
+    font-weight: normal;
+}
+
+#article h1 {
+    font-size: 1.4em;
+}
+
+#article h2 {
+    font-size: 1.2em;
+}
+
+#article h3 {
+    font-size: 1.1em;
+}
+
+#article-list > li {
+    list-style: square inside;
+    padding-bottom: 2px;
+    margin-bottom: 4px;
+}
+
+.article-footer {
+	clear: both;
+	float: right;
+	margin-bottom: 1.5em;
+}
+
+.article-meta {
+    float: right;
+}
+
+/* Category list after an article */
+#article-category-list > #category-list {
+	float: right;
+	margin-top: 0;
+}
+
+.clear, hr {
+    clear: both;
+}
+`
+
+const baseTemplateText = `<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>
+        {{ .BlogTitle }} » 
+        {{ if .IsCategory }}
+            {{ index .CategoryMap .Category }}
+        {{ else }}
+            {{ .Title }}
+        {{ end }}
+    </title>
+    <style> {{ .CSS }} </style>
+</head>
+<body>
+    <div id="page">
+        <header id="header"><a href="{{ .Root }}/index.html">{{ .BlogTitle }}</a></header>
+        {{ template "content" . }}
+    </div>
+</body>
+</html>
+
+{{ define "category-list" }}
+	{{ $isCat := .IsCategory }}
+	{{ $cat := .Category }}
+	{{ $root := .Root }}
+	<ul id="category-list">
+		{{ range $info := .Categories }}
+			<li><a href="{{ $root }}/{{ $info.Name }}/index.html"
+				   class="nav-link {{ if and $isCat (eq $cat $info.Name) }}current{{ end }}">
+				{{ $info.Title }}
+			</a></li>
+		{{ end }}
+	</ul>
+{{ end }}
+
+{{ define "article-list" }}
+	<ul id="article-list">
+		{{ range $info := .Articles }}
+			<li>
+				<a href="{{ $info.Name }}.html" class="nav-link">{{ $info.Title }}</a> 
+				<span class="article-meta">{{ $info.Timestamp }}</span>
+			</li>
+		{{ end }}
+	</ul>
+{{ end }}
+`
+
+const articleTemplateText = `{{ define "content" }}
+	<article id="article">
+		<h1>{{ .Title }}</h1>
+		{{ .Content }}
+	<div class="clear"></div>
+	<span class="article-footer">
+		<a href="{{ .Root }}/{{ .Category }}/index.html" class="nav-link">
+			{{ index .CategoryMap .Category }}
+		</a>
+		·
+		{{ .Timestamp }}</span>
+	</article>
+	<hr>
+	{{ if .IsHomepage }}
+		<div class="recent">
+			{{ .RecentTitle }}
+			{{ template "article-list" . }}
+		</div>
+		<hr>
+	{{ end }}
+	<div id="article-category-list">
+		{{ .CategoryListTitle }}
+		{{ template "category-list" . }}
+	</div>
+	<div class="clear"></div>
+{{ end }}
+`
+
+const categoryTemplateText = `{{ define "content" }}
+	{{ template "category-list" . }}
+	{{ template "article-list" . }}
+{{ end }}
+`
---
+++ main.go
@@ -0,0 +1,211 @@
+//go:generate ./gen-include
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"text/template"
+
+	"github.com/BurntSushi/toml"
+)
+
+type blogConf struct {
+	Title      string
+	Categories []categoryMeta
+	Recent int
+	RecentTitle string
+	CategoryListTitle string
+}
+
+type categoryConf struct {
+	Articles []articleMeta
+}
+
+type baseDot struct {
+	BlogTitle  string
+	RecentTitle string
+	CategoryListTitle string
+	Categories []categoryMeta
+
+	CategoryMap map[string]string
+	CSS         string
+}
+
+type categoryMeta struct {
+	Name  string
+	Title string
+}
+
+func newBaseDot(t, rt, clt string, cs []categoryMeta) *baseDot {
+	b := &baseDot{t, rt, clt, cs, make(map[string]string), css}
+	for _, m := range cs {
+		b.CategoryMap[m.Name] = m.Title
+	}
+	return b
+}
+
+func (b *baseDot) IsCategory() bool {
+	return false
+}
+
+func (b *baseDot) IsHomepage() bool {
+	return false
+}
+
+func (b *baseDot) Root() string {
+	return ".."
+}
+
+type articleMeta struct {
+	Name      string
+	Title     string
+	Timestamp string
+}
+
+type articleDot struct {
+	baseDot
+	articleMeta
+	Category string
+	Content  string
+}
+
+type categoryDot struct {
+	baseDot
+	Category string
+	Articles []articleMeta
+}
+
+func (c *categoryDot) IsCategory() bool {
+	return true
+}
+
+const recentArticles = 5
+
+type homepageDot struct {
+	articleDot
+	Articles []articleMeta
+}
+
+func (h *homepageDot) insertNewArticle(am articleMeta, nmax int) {
+	var i int
+	for i = len(h.Articles); i > 0; i-- {
+		if h.Articles[i-1].Timestamp < am.Timestamp {
+			break
+		}
+	}
+	if i == len(h.Articles) {
+		if i < nmax {
+			h.Articles = append(h.Articles, am)
+		}
+		return
+	}
+
+	if len(h.Articles) < nmax {
+		h.Articles = append(h.Articles, articleMeta{})
+	}
+
+	copy(h.Articles[i+1:], h.Articles[i:])
+	h.Articles[i] = am
+}
+
+func (h *homepageDot) IsHomepage() bool {
+	return true
+}
+
+func (h *homepageDot) Root() string {
+	return "."
+}
+
+func decodeFile(fname string, v interface{}) {
+	_, err := toml.DecodeFile(fname, v)
+	if err != nil {
+		log.Fatalf("when reading %v: %v", fname, err)
+	}
+}
+
+func openForWrite(fname string) (*os.File, error) {
+	return os.OpenFile(fname, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Fprintln(os.Stderr, "Usage: genblog <src dir> <dst dir>")
+		os.Exit(1)
+	}
+	srcDir := os.Args[1]
+	dstDir := os.Args[2]
+
+	bf := &blogConf{}
+	decodeFile(path.Join(srcDir, "index.toml"), &bf)
+	bd := newBaseDot(bf.Title, bf.RecentTitle, bf.CategoryListTitle, bf.Categories)
+
+	categoryTmpl := template.New("category")
+	template.Must(categoryTmpl.Parse(baseTemplateText))
+	template.Must(categoryTmpl.Parse(categoryTemplateText))
+
+	articleTmpl := template.New("article")
+	template.Must(articleTmpl.Parse(baseTemplateText))
+	template.Must(articleTmpl.Parse(articleTemplateText))
+
+	homepage := &homepageDot{}
+
+	for _, cm := range bf.Categories {
+		cf := &categoryConf{}
+		decodeFile(path.Join(srcDir, cm.Name, "index.toml"), &cf)
+
+		cDir := path.Join(dstDir, cm.Name)
+		// Create directory
+		err := os.MkdirAll(cDir, 0755)
+		if err != nil {
+			log.Fatalf("failed to create output dir for %v: %v\n", cm.Name, err)
+		}
+
+		// Generate index
+		file, err := openForWrite(path.Join(cDir, "index.html"))
+		if err != nil {
+			log.Fatalf("failed to open index of %v: %v\n", cm.Name, err)
+		}
+		defer file.Close()
+		cd := &categoryDot{*bd, cm.Name, cf.Articles}
+		err = categoryTmpl.Execute(file, cd)
+		if err != nil {
+			log.Fatalf("failed to render index of %v: %v\n", cm.Name, err)
+		}
+
+		// Generate articles
+		for _, am := range cf.Articles {
+			// Read article
+			content, err := ioutil.ReadFile(path.Join(srcDir, cm.Name, am.Name+".html"))
+			if err != nil {
+				log.Fatalf("failed to read article %v/%v: %v\n", cm.Name, am.Name, err)
+			}
+
+			// Generate article
+			file, err := openForWrite(path.Join(cDir, am.Name+".html"))
+			if err != nil {
+				log.Fatalf("failed to open article %v/%v: %v\n", cm.Name, am.Name, err)
+			}
+			defer file.Close()
+			ad := &articleDot{*bd, am, cm.Name, string(content)}
+			err = articleTmpl.Execute(file, ad)
+			if err != nil {
+				log.Fatalf("failed to render article %v/%v: %v\n", cm.Name, am.Name, err)
+			}
+
+			homepage.insertNewArticle(ad.articleMeta, bf.Recent)
+
+			if homepage.Timestamp < ad.Timestamp {
+				homepage.articleDot = *ad
+			}
+		}
+	}
+	// Generate homepage
+	file, err := openForWrite(path.Join(dstDir, "index.html"))
+	err = articleTmpl.Execute(file, homepage)
+	if err != nil {
+		log.Fatalf("failed to render homepage: %v\n", err)
+	}
+}
