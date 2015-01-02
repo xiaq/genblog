98550888a836e8f02b0e03ba34cac6c922f191c5
Cheer Xiao <xiaqqaix@gmail.com>
2015-01-02 23:31:33 +0100

    Should have go-generated before push, oops

--- include.go
+++ include.go
@@ -147,6 +147,8 @@ const baseTemplateText = `<!doctype html>
 <html>
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+
     <title>
         {{ .BlogTitle }} » 
         {{ if .IsCategory }}
