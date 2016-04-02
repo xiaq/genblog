493015ddb7aeec007a8c210c88cb2162caed0d63
Qi Xiao <xiaqqaix@gmail.com>
2016-04-02 17:48:48 +0200

    Make sort.go more compact.

--- sort.go
+++ sort.go
@@ -2,20 +2,12 @@ package main
 
 import "sort"
 
-type sortInterface []articleMeta
+type articleMetas []articleMeta
 
-func (si sortInterface) Len() int {
-	return len(si)
-}
-
-func (si sortInterface) Less(i, j int) bool {
-	return si[i].Timestamp > si[j].Timestamp
-}
-
-func (si sortInterface) Swap(i, j int) {
-	si[i], si[j] = si[j], si[i]
-}
+func (si articleMetas) Len() int           { return len(si) }
+func (si articleMetas) Less(i, j int) bool { return si[i].Timestamp > si[j].Timestamp }
+func (si articleMetas) Swap(i, j int)      { si[i], si[j] = si[j], si[i] }
 
 func sortArticleMetas(ams []articleMeta) {
-	sort.Sort(sortInterface(ams))
+	sort.Sort(articleMetas(ams))
 }
